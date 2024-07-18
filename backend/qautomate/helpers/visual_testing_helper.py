38595d2ca159fc83f94108ea556f5959a6b18357
Krish Mehta <me@krishvmehta.com>
2024-07-18 13:26:42 +0200

    Refactor visual_testing_helper.py, simplify syntax, and improve code quality

--- backend/qautomate/helpers/visual_testing_helper.py
+++ backend/qautomate/helpers/visual_testing_helper.py
@@ -1,13 +1,10 @@
-import base64
-from openai import OpenAI
 import os
+from openai import OpenAI
 
 # Read the value of the environment variable
 openai_key = os.getenv('openai_key')
 if not openai_key:
     raise ValueError("No OpenAI API key found in environment variable 'openai_key'")
-
-
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY", openai_key))
 
 
@@ -33,20 +30,21 @@ def analyze_image_color(base64_image, model="gpt-4o", max_tokens=4096):
                         1. The colors used for each UI element.
 
                         I will share the image of the application. Please analyze it carefully and describe the color details and overall color scheme.
-                        The image is shared.
-                        '''
+                        
+                        The image is shared.'''
                      },
                     {
                         "type": "image_url",
                         "image_url": {
                             "url": f"{base64_image}",
                             "detail": "high"
-                        },
-                    },
-                ],
+                        }
+                    }
+                ]
             }
         ],
-        max_tokens=max_tokens, )
+        max_tokens=max_tokens,
+    )
     return response.choices[0].message.content
 
 
@@ -65,12 +63,12 @@ def difference_in_colors_analyze(description_image_1, description_image_2, base6
                     My goal is to identify differences in two images.
                     I will share the image description and corresponding images and you give me all differences between them. 
 
-                    please do it carefully as it is critical.
+                    Please do it carefully as it is critical.
 
-                description of Original Image : {description_image_1}
-                description of Testing Image : {description_image_2}
+                Description of Original Image: {description_image_1}
+                Description of Testing Image: {description_image_2}
 
-                example structure:
+                Example structure:
 
                 #### 1. **Navigation Bar Icons (Top Menu)**
                 - **Original Image:**
@@ -97,22 +95,21 @@ def difference_in_colors_analyze(description_image_1, description_image_2, base6
                   - **Close Button (X)**: Black (#000000)
                   - **Day After Button**: Green background (#7FC242) with white text.
                   - **Day After Button**: Green background (#7FC242) with white text.
-
               """
                     },
                     {
                         "type": "image_url",
                         "image_url": {
                             "url": f"{base64_image_1}",
-                        },
+                        }
                     },
                     {
                         "type": "image_url",
                         "image_url": {
                             "url": f"{base64_image_2}",
-                        },
-                    },
-                ],
+                        }
+                    }
+                ]
             }
         ],
         max_tokens=4096,
@@ -122,11 +119,10 @@ def difference_in_colors_analyze(description_image_1, description_image_2, base6
     return response_of_color.choices[0].message.content
 
 
-# Paths to your images
-def process_color_in_images(originalImage, testingImage):
+def process_color_in_images(original_image, testing_image):
     print("processing color in images")
 
-    encoded_images = [originalImage, testingImage]
+    encoded_images = [original_image, testing_image]
 
     # Analyze each image
     descriptions = []
@@ -141,8 +137,8 @@ def process_color_in_images(originalImage, testingImage):
     description_image_2_color = descriptions[1]
 
     # difference in image based on color
-    color_difference = difference_in_colors_analyze(description_image_1_color, description_image_2_color, encoded_images[0],
-                                                    encoded_images[1])
+    color_difference = difference_in_colors_analyze(description_image_1_color, description_image_2_color,
+                                                    encoded_images[0], encoded_images[1])
     return color_difference
 
 
@@ -155,33 +151,33 @@ def analyze_image_layout(base64_image, model="gpt-4o", max_tokens=4096):
                 "content": [
                     {"type": "text",
                      "text": '''
-                        Act as an expert QA in visual testing for Android applications. Your task is to analyze an image of an Android application's interface to understand the layout of its UI elements.
+                     Act as an expert QA in visual testing for Android applications. Your task is to analyze an image of an Android application's interface to understand the layout of its UI elements.
 
                     Please provide a detailed report that includes:
                     1. The location of each UI element within the image.
 
                     I will share the image of the application. Please analyze it thoroughly and describe the layout comprehensively.
                     Dont share any color related information but only elements layout related analysis.
-                        The image is shared.
-                        '''
+                        
+                    The image is shared.'''
                      },
                     {
                         "type": "image_url",
                         "image_url": {
                             "url": f"{base64_image}",
                             "detail": "high"
-                        },
-                    },
-                ],
+                        }
+                    }
+                ]
             }
         ],
         max_tokens=max_tokens,
         temperature=0.3,  # Lower temperature for more deterministic and precise responses
         top_p=0.6)
+
     return response.choices[0].message.content
 
 
-# Function to analyze differences in layout
 def difference_in_layout_analyze(description_image_1, description_image_2, base64_image_1, base64_image_2):
     response = client.chat.completions.create(
         model="gpt-4o",
@@ -192,17 +188,16 @@ def difference_in_layout_analyze(description_image_1, description_image_2, base6
                     {
                         "type": "text",
                         "text": f"""
-                    Act as an expert QA in visual testing for Android applications., your task is to compare two image
+                Act as an expert QA in visual testing for Android applications., your task is to compare two image
                 of an android application's interface. 
                 My goal is to identify differences in two images.
                 I will share the image description and corresponding images and you give me all differences between them. 
 
-                please do it carefully as it is critical.
+                Please do it carefully as it is critical.
 
-                description of Original Image : {description_image_1}
-                description of Testing Image : {description_image_2}
-
-              """
+                Description of Original Image : {description_image_1}
+                Description of Testing Image : {description_image_2}
+                """
                     },
                     {
                         "type": "image_url",
@@ -214,9 +209,9 @@ def difference_in_layout_analyze(description_image_1, description_image_2, base6
                         "type": "image_url",
                         "image_url": {
                             "url": f"{base64_image_2}",
-                        },
-                    },
-                ],
+                        }
+                    }
+                ]
             }
         ],
         max_tokens=4096,
@@ -227,11 +222,11 @@ def difference_in_layout_analyze(description_image_1, description_image_2, base6
 
 
 # Analyze each image
-def process_layout_in_images(originalImage, testingImage):
+def process_layout_in_images(original_image, testing_image):
     print("processing layout of images")
 
     descriptions = []
-    encoded_images = [originalImage, testingImage]
+    encoded_images = [original_image, testing_image]
     for image in encoded_images:
         description = analyze_image_layout(image)
         descriptions.append(description)
@@ -267,18 +262,16 @@ def analyze_image_text(base64_image, model="gpt-4o", max_tokens=4096):
                         I will share the image of the application. Please analyze it carefully and provide accurate details.
                         Dont give information about color or layout but specifically about the UI element and text in UI element
 
-
-                    image is shared
-                        '''
+                        Image is shared'''
                      },
                     {
                         "type": "image_url",
                         "image_url": {
                             "url": f"{base64_image}",
                             "detail": "high"
-                        },
-                    },
-                ],
+                        }
+                    }
+                ]
             }
         ],
         max_tokens=max_tokens,
@@ -287,7 +280,6 @@ def analyze_image_text(base64_image, model="gpt-4o", max_tokens=4096):
     return response.choices[0].message.content
 
 
-# Function to analyze differences in text
 def difference_in_text_analyze(description_image_1, description_image_2, base64_image_1, base64_image_2):
     response = client.chat.completions.create(
         model="gpt-4o",
@@ -298,42 +290,40 @@ def difference_in_text_analyze(description_image_1, description_image_2, base64_
                     {
                         "type": "text",
                         "text": f"""
-                    Act as an expert QA in visual testing for Android applications., your task is to compare two image
+                Act as an expert QA in visual testing for Android applications., your task is to compare two image
                 of an android application's interface. 
                 My goal is to identify differences in two images.
                 I will share the image description and corresponding images and you give me all differences between them. 
 
-                please do it carefully as it is critical.
+                Please do it carefully as it is critical.
 
-            description of Original Image : {description_image_1}
-            description of Testing Image : {description_image_2}
+            Description of Original Image: {description_image_1}
+            Description of Testing Image: {description_image_2}
 
-            example structure:
+            Example structure:
 
             #### 1. **Navigation Bar Icons (Top Menu)**
             - **Original Image:**
             - **Testing Image:**
 
-
             #### 2. **Date Selection:**
             - **Original Image:**
             - **Testing Image:**
-
-              """
+            """
                     },
                     {
                         "type": "image_url",
                         "image_url": {
                             "url": f"{base64_image_1}",
-                        },
+                        }
                     },
                     {
                         "type": "image_url",
                         "image_url": {
                             "url": f"{base64_image_2}",
-                        },
-                    },
-                ],
+                        }
+                    }
+                ]
             }
         ],
         max_tokens=4096,
@@ -343,12 +333,11 @@ def difference_in_text_analyze(description_image_1, description_image_2, base64_
     return response.choices[0].message.content
 
 
-# Analyze each image for text
-def process_text_in_images(originalImage, testingImage):
+def process_text_in_images(original_image, testing_image):
     print("processing text of images")
 
     descriptions = []
-    encoded_images = [originalImage, testingImage]
+    encoded_images = [original_image, testing_image]
     for image in encoded_images:
         description = analyze_image_text(image)
         descriptions.append(description)
@@ -395,17 +384,16 @@ def visual_analyze(difference_in_text, difference_in_layout, difference_in_color
                     
                     Summary:
                   
-                    ###Top Navigation Bar:
+                    ### Top Navigation Bar:
                     Original Image: Features icons for Train, Flight, Bus, and Hotel, with promotional text "UPTO 30% OFF."
                     Testing Image: Similar icons but shuffled order and additional promotional texts like "Save ₹500" and "Min. 15% off."
                     /n/n
-                    ###Search Section:
+                    ### Search Section:
                     Original Image: Displays black text on a white background for station names NDLS - New Delhi and LKO - Lucknow Nr, with labels such as “Wednesday, 19 Jun”, and a bright blue "Search Trains" button.
                     Testing Image: Uses black text on a yellow background for station names NDLS and PNBE - Patna Jn, with labels such as “Monday, 13 Sep” and a "Search Flights" button.
-                    /n/n
-              """
-                    },
-                ],
+                    /n/n"""
+                    }
+                ]
             }
         ],
         max_tokens=4096,
@@ -413,8 +401,3 @@ def visual_analyze(difference_in_text, difference_in_layout, difference_in_color
         top_p=0.6)
 
     return response.choices[0].message.content
-
-
-
-
-
